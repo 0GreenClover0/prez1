31f14ddfdff8463bfff542ec38da4a9f26ccfc7f
Mikolaj Przybylski <clovers02123@gmail.com>
2024-11-12 01:11:39 +0100

    Make cube smaller over time while disappearing

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -12,6 +12,9 @@ public class Test : MonoBehaviour
         Vector3 newPosition = transform.position + new Vector3(Time.deltaTime * 10f, 0.0f, 1.0f / timer);
         transform.position = newPosition;
 
+        float scale = transform.localScale.x - 0.001f;
+        transform.localScale = new Vector3(scale, scale, scale);
+
         timer -= Time.deltaTime;
     }
 }
